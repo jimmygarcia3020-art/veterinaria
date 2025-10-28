014ac801dd826dbecd8eefa1d4e802e3d656e938
Jota_G.C <LAB-USR-CHIMBOTE@grupoutp.net>
2025-10-28 10:25:42 -0500

    Initial commit

---
+++ src/main/java/com/example/veterinaria/controller/MascotaController.java
@@ -0,0 +1,44 @@
+package com.example.veterinaria.controller;
+
+import com.example.veterinaria.model.Mascota;
+import com.example.veterinaria.service.MascotaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/mascota")
+public class MascotaController {
+    private final MascotaService mascotaService;
+
+    public MascotaController (MascotaService mascotaService){
+        this.mascotaService = mascotaService;
+    }
+
+    @GetMapping
+    public List<Mascota> obtenerMascotas(){
+        return mascotaService.listarTodas();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Mascota> obtenerMascotaPorId(@PathVariable Integer id){
+        return mascotaService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<?>
+    crearMascota(@RequestBody Mascota mascota){
+        if(mascota.getNombre() == null || mascota.getNombre().trim().isEmpty()){
+            return ResponseEntity.badRequest().body("Elcampo es obligatorio");
+        }
+        if(mascota.getEdad() == null){
+            return ResponseEntity.badRequest().body("Elcampo es obligatorio");
+        }
+        if(mascota.getEspecie() == null || mascota.getEspecie().trim().isEmpty()){
+            return ResponseEntity.badRequest().body("Elcampo es obligatorio");
+        }
+        return ResponseEntity.ok(mascotaService.crearMascota(mascota));
+    }
+}
